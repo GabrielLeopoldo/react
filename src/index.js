6e897410b086831045e27a8746e22409e9bfbb3f
aluno <aluno@treinamento.(none)>
2016-09-14 17:16:46 -0300

    Modularize app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from 'react'
 import { render } from  'react-dom'
-import Podcast from './podcasts'
-import Player from './player'
-import MyFeeds from './my_feeds'
-
-const App = function(props){
-    return <section id="podcasts-app">
-        <section id="stage">
-          <MyFeeds /> 
-          <Podcast /> 
-        </section>
-        <Player />
-      </section>
-}
+import App from './components/app'
 
 render(<App />, document.getElementById('main-app'))
